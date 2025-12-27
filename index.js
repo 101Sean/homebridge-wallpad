046a7685ba9df386300cf52818136456830d4569
mikeryu98 <mikeryu98@gmail.com>
2025-12-28 00:56:34 +0900

    v1.0.1

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class WallpadAccessory {
         this.tcpClient = null;
 
         this.setupServices();
-        //this.connectToEW11();
+        //임시 this.connectToEW11();
     }
 
     setupServices() {
@@ -88,7 +88,8 @@ class WallpadAccessory {
     async handleLockTargetStateSet(value) {
         if (value === 0) {
             this.log.info('[명령] 공동현관 개방 패킷 전송');
-            /*const packet = this.config.openPacket || 'AA550102000103';
+            /* 임시
+            const packet = this.config.openPacket || 'AA550102000103';
             if (this.tcpClient && !this.tcpClient.destroyed) {
                 this.tcpClient.write(Buffer.from(packet, 'hex'));
             }*/
